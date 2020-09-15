c6d9339086531fa8bd3a873cd3d0c903d5397bd3
susy <susy@tensynchina.com>
2020-09-15 20:15:04 +0800

    log

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1076,7 +1076,7 @@ exports.projectTrackDELETE = (req, res, next) => {
 };
 
 exports.renderCanvas = (req, res, next) => {
-	console.log("renderCanvas req:", req, ",res:", res, ",next:", next);
+	console.log("renderCanvas res:",res:", res, ",next:", next);
 	for(var k in req){
 		if(k=="bytes")continue;
 		console.log(k,"=", req[k]);
